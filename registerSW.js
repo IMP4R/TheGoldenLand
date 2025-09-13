821f2c03f138f8a05600555a79eace9082fffa6f
Денис Лузянин <stud204055@vyatsu.ru>
2025-09-13 22:05:21 +0300

    Initial commit: Telegram Notepad PWA

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,11 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
